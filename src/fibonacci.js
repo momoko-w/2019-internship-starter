648c966e60138ae86ffcd18d29d2cf4f1571d183
momoko-w <momoko.werner@gmail.com>
2019-04-06 23:34:48 +0100

    Passing multiplication table tests

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -2,13 +2,14 @@ function getSequence(n) {
   // TODO: Generate a sequence of Fibonacci numbers
   var fibNumbers = [1], intNew = 1, intOld = 0, intZ, i;
 
-  //loop through n times to get sequence of length n
-  for (i = 0; i <= n-2; i++) {
+  //loop through to calculate sequence
+  for (i = 0; i < n-1; i++) {
     //store current temporarily, update current number, store previous current as old
     intTemp = intNew;
     intNew = intNew + intOld;
     intOld = intTemp;
 
+    //add newly calculated digit to end of array
     fibNumbers.push(intNew);
 
   }
--- src/tableFormatter.js
+++ src/tableFormatter.js
@@ -1,8 +1,33 @@
 //Format the multiplication table output to align the numbers evenly, accounting for numbers having different numbers of digits
 
+//Note: table variable passed in is 2D array of rows
 function format(table) {
+
+  var stringRow = []; //since "row" is array of numbers
+
+  //get value of last row and last column in the table -> greatest ("longest") number in table
+  var longNum = table[table.length-1][table.length-1];
+
+  //get maximum length of what numbers have to be padded up to
+  var paddingLength = longNum.toString().length;
+
+  //applying function below on each element of the 2D array (rows)
   const tableText = table.map(function(row) {
-    return row.join(" ");
+
+    //traverse through row elements
+    for (var i = 0; i < row.length; i++) {
+
+      var value = row[i].toString();
+
+      //add padding spaces to string value
+      while (value.length < paddingLength){
+        value = " " + value;
+      }
+
+      stringRow[i] = value;
+    }
+
+    return stringRow.join(" ");   
   });
 
   return "\n" + tableText.join("\n") + "\n";
